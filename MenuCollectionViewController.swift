2b8f6a40120d1fb767ffe11c898e2787ef58fc39
Mike Reining <emailmike@gmail.com>
2015-02-19 20:59:29 -0800

    Added UICollectionViewDelegateFlowLayout and tweaked func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets { to center cells

--- MenuCollectionViewController.swift
+++ MenuCollectionViewController.swift
@@ -8,25 +8,48 @@
 
 import UIKit
 
-class MenuCollectionViewController: UICollectionViewController {
+class MenuCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
+    
+    override func viewDidLoad() {
+        collectionView!.contentInset = UIEdgeInsets(top: 100, left: 0, bottom: 0, right: 0)
 
+    }
+    
     // MARK: UICollectionViewDataSource
     
     override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
-        return 1
+        return 2
     }
     
     
     override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //#warning Incomplete method implementation -- Return the number of items in the section
-        return 6
+        switch section {
+        case 0:     return 2
+        case 1:     return 3
+        default:    return 0
+        }
     }
-override     
-    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("CollectionCell", forIndexPath: indexPath) as UICollectionViewCell
         println(indexPath.item)
         // Configure the cell
         return cell
     }
 
+    // MARK: UICollectionViewDelegateFlowLayout
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAtIndex section: Int) -> UIEdgeInsets {
+        var numberOfCellsInSection = collectionView.numberOfItemsInSection(section)
+        let layout = collectionViewLayout as UICollectionViewFlowLayout
+        let cellWidth = layout.itemSize.width
+        let collectionViewWidth = collectionView.bounds.width
+        let cellPadding = layout.minimumInteritemSpacing
+        let requiredInset = (collectionViewWidth - (cellWidth * CGFloat(numberOfCellsInSection)) - (cellPadding * CGFloat(numberOfCellsInSection))) / 2
+        let edgeInsets = UIEdgeInsets(top: 0, left: requiredInset, bottom: 50, right: requiredInset)
+        return edgeInsets
+    }
+    
+    
 }
