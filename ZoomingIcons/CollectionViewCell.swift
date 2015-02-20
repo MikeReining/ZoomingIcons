66e334a6597f5eb8e7e2ed59d22934c45bb34ea5
Mike Reining <emailmike@gmail.com>
2015-02-19 21:08:10 -0800

    Made collection view cells circular

--- ZoomingIcons/CollectionViewCell.swift
+++ ZoomingIcons/CollectionViewCell.swift
@@ -13,7 +13,11 @@ class CollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var colorView: UIView!
     @IBOutlet weak var imageView: UIImageView!
  
-
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        colorView.layer.cornerRadius = bounds.width / 2 // 50 since cell is 100 wide
+        colorView.layer.masksToBounds = true // not necessary since already setup in storyboard via autolayout
+    }
     
     
 }
---
+++ ZoomingIcons/Icon.swift
@@ -0,0 +1,9 @@
+//
+//  Icon.swift
+//  ZoomingIcons
+//
+//  Created by Michael Reining on 2/19/15.
+//  Copyright (c) 2015 Mike Reining. All rights reserved.
+//
+
+import Foundation
